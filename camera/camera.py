c8135e45f1528249ee3167d3e8459bde23ef127e
hugo brisebois <134088623+HugoBrisebois@users.noreply.github.com>
2026-08-24 15:58:59 -0400

    trying to configure the pi camera for fine control and basic use

--- camera/camera.py
+++ camera/camera.py
@@ -1,4 +1,10 @@
 from picamera2 import Picamera2, Preview
 
+
+# configuring the camera for start
 picam = Picamera2()
-picam.start_preview(Preview.DRM)
+config = picam.create_preview_configuration()
+picam.configure(config)
+
+# start the pi camera module  
+picam.start()
