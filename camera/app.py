81a442d3755c118f8d1618d0ad0af44821605ddf
HugoBrisebois <Hugo.Brisebois@gmail.com>
2026-09-04 15:32:02 -0400

    Add camera functionality with capture_image placeholder and create index.html template

--- camera/app.py
+++ camera/app.py
@@ -1,6 +1,31 @@
+# web app imports
 from flask import Flask
 from flask import render_template
 from markupsafe import escape
+
+# camera imports
+from picamera2 import Picamera2
+from libcamera import controls
+import time
+
+def capture_image():
+    pass    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app = Flask(__name__)
 
