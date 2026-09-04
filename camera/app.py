efb71ac0d77758c5627a45cb6a2ea13cf8bc336f
HugoBrisebois <Hugo.Brisebois@gmail.com>
2026-09-04 15:48:01 -0400

    Implement capture_image function to configure and capture images from the Raspberry Pi camera

--- camera/app.py
+++ camera/app.py
@@ -9,19 +9,40 @@ from libcamera import controls
 import time
 
 def capture_image():
-    pass    
+    # configuring the rpi camera module 3
+    # to be able to capture and display the various images 
+    # captured by the camera
+    picam = Picamera2()
+    capture_config = picam.create_still_configuration(main={"size": (1280, 960)})
 
 
+    # set global variables for the camera settings
+    exposure = 2000000   # 2 second frameing preview
+    gain = 8.0       # amplify the sensor signal
+
+    picam.set_controls({
+        "AfMode": controls.AfModeEnum.Manual,   
+        "LensPosition": 0.0,      # Set Focus to infinity
+        "AeEnable": False,        # Enable manual shutter mode so that the camera doesn't auto adjust brightness
+        "ExposureTime": exposure,
+        "AnalogueGain": gain
+    })
+
+    picam.start()
 
 
+    # capturing an image
+
+    time.sleep(1)
+    image = picam.capture_file("test.jpeg", format='jpeg')
 
 
+    # stop the camera
+    picam.stop()    
 
-
-
-
-
-
+def stream_video():
+    # make the camera stream the video to a flask web app
+    pass
 
 
 
